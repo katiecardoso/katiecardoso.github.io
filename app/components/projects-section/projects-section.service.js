dde8251f83f4d1510f5d1b4b810c366a9db4c259
Nick Makes <nick.makes@mlb.com>
2017-05-23 08:43:21 -0400

    Adds black border around hot dog book cover

--- app/components/projects-section/projects-section.service.js
+++ app/components/projects-section/projects-section.service.js
@@ -47,7 +47,8 @@
         this.hotDog = {
                 title : "How the Hot Dog Found its Bun",
                 blurb : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                thumbnail : "resources/images/covers/hot_dog.jpg"
+                thumbnail : "resources/images/covers/hot_dog.jpg",
+                border : "black"
         };
 
         this.kidsOutdoorAdventureBook = {
